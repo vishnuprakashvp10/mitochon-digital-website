a2733feb3e6ad29cfb4b82d6bb3ae7c45c7fc9ab
S Vishnuprakash <vishnuprakashvp03@gmail.com>
2024-06-10 19:35:52 +0530

    removed-tag-manager

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,6 @@ import Header from "@/constant/Navbar/Header";
 import BottomBar from "@/constant/BottomBar/BottomBar";
 import WhatsApp from "@/constant/FAB/WhatsApp";
 import Head from "next/head";
-import { Toaster } from "react-hot-toast";
 
 const nunito = Nunito({
 	weight: ["200", "300", "400", "500", "600", "700", "800"],
@@ -20,46 +19,19 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-	const gtmScript = `
-		(function(w,d,s,l,i){
-			w[l]=w[l]||[];
-			w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-			var f=d.getElementsByTagName(s)[0],
-			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-			j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-			f.parentNode.insertBefore(j,f);
-		})(window,document,'script','dataLayer','GTM-KQ5399G4');
-	`;
-
 	return (
 		<html lang="en">
-			<Head>
-				{/* Google Tag Manager */}
-				<script dangerouslySetInnerHTML={{ __html: gtmScript }} />
-				{/* End Google Tag Manager */}
+			<head>
 				<meta name="fl-verify" content="9be66eef7379e3946a735731ecf2c665" />
 				<link rel="icon" href="/favicon.png" />
-			</Head>
+			</head>
 			<body className={nunito.className}>
-				{/* Google Tag Manager (noscript) */}
-				<noscript>
-					<iframe
-						src="https://www.googletagmanager.com/ns.html?id=GTM-KQ5399G4"
-						height="0"
-						width="0"
-						style={{ display: 'none', visibility: 'hidden' }}
-					></iframe>
-				</noscript>
-				{/* End Google Tag Manager (noscript) */}
 				<Header />
 				{children}
 				<BottomBar />
 				<WhatsApp />
 				<Footer />
-				<Toaster />
 			</body>
 		</html>
 	);
 }
-
-
