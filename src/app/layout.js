104f71b4b43304806c488dad2d7d67c8e40f8d92
S Vishnuprakash <vishnuprakashvp03@gmail.com>
2024-06-10 19:29:16 +0530

    tag-manager-added

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,13 +20,37 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+	const gtmScript = `
+		(function(w,d,s,l,i){
+			w[l]=w[l]||[];
+			w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+			var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+			j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+			f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-KQ5399G4');
+	`;
+
 	return (
 		<html lang="en">
-			<head>
+			<Head>
+				{/* Google Tag Manager */}
+				<script dangerouslySetInnerHTML={{ __html: gtmScript }} />
+				{/* End Google Tag Manager */}
 				<meta name="fl-verify" content="9be66eef7379e3946a735731ecf2c665" />
 				<link rel="icon" href="/favicon.png" />
-			</head>
+			</Head>
 			<body className={nunito.className}>
+				{/* Google Tag Manager (noscript) */}
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-KQ5399G4"
+						height="0"
+						width="0"
+						style={{ display: 'none', visibility: 'hidden' }}
+					></iframe>
+				</noscript>
+				{/* End Google Tag Manager (noscript) */}
 				<Header />
 				{children}
 				<BottomBar />
