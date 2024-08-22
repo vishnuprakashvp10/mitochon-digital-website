8abfaee74e36814742978d8c74a5b9d6430f0ef9
S Vishnuprakash <vishnuprakashvp03@gmail.com>
2024-08-22 18:49:16 +0530

    now

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -31,6 +31,26 @@ const Home = () => {
 					</div>
 				</div>
 			</div>
+
+			<div className="absolute circle-animation rounded-full animate-[spin_4s_linear_infinite] z-1">
+				<div className="circle-animation-child rounded-full"></div>
+			</div>
+			<div className="absolute half-circle-animation rounded-full animate-[spin_3s_linear_infinite] z-1">
+				<div className="half-circle-animation-child rounded-full"></div>
+			</div>
+			<div className="absolute square-animation animate-[spin_3s_linear_infinite] z-1">
+				<div className="square-animation-child"></div>
+			</div>
+			<div className="absolute bubble-animation flex h-3 w-3 z-1">
+				<span className="animate-[ping_1s_linear_infinite] absolute inline-flex h-full w-full rounded-full bg-yellow-400 opacity-75"></span>
+				<span className="relative inline-flex rounded-full h-3 w-3 bg-yellow-500"></span>
+			</div>
+			<div className="absolute square-animation-2 animate-[spin_2s_linear_infinite] z-1">
+				<div className="square-animation-child-2"></div>
+			</div>
+			<div className="absolute square-animation-3 animate-[spin_3s_linear_infinite] z-1">
+				<div className="square-animation-child-3"></div>
+			</div>
 		</section>
 	);
 };
