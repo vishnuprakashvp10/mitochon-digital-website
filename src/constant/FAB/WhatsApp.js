580f880fe1746cb70ead73990924a1f9992dad97
S Vishnuprakash <vishnuprakashvp03@gmail.com>
2025-01-16 13:16:44 +0530

    changes-whatsapp

--- src/constant/FAB/WhatsApp.js
+++ src/constant/FAB/WhatsApp.js
@@ -5,18 +5,13 @@ const WhatsApp = () => {
   return (
     <>
       <a
-        className="fixed bottom-20 -right-0 h-12 w-12 rounded-full bg-[#29A71A] flex items-center justify-center"
+        className="fixed bottom-28 right-4 lg:right-8 md:bottom-8 z-20 drop-shadow-lg"
         href="https://wa.me/+447442005437"
       >
-        <Image src={image.whatsapp} height={45} width={45} alt="whats app" />
+        <Image src={image.whatsapp} height={60} width={60} alt="whats app" />
       </a>
     </>
   );
 };
 
 export default WhatsApp;
-
-
-      {/*    <a className="fixed bottom-24 right-4 md:right-8 md:bottom-8 z-20 drop-shadow-lg hidden md:block" href="https://wa.me/+447442005437">
-    <Image src={image.whatsapp} height={50} width={50} alt="whats app"/>
-  </a> */}
