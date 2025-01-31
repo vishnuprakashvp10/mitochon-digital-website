7ed1872da3ea8c793eb250e351d9c82255d4f409
S Vishnuprakash <vishnuprakashvp03@gmail.com>
2025-01-31 22:05:06 +0530

    mailjs

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -1,25 +1,36 @@
+// import nodemailer from "nodemailer";
+// import smtpTransport from "nodemailer-smtp-transport";
+
+// const email = process.env.EMAIL;
+// const pass = process.env.EMAIL_PASS;
+// export const transporter = nodemailer.createTransport(
+// 	smtpTransport({
+// 		host: "smtpout.secureserver.net",
+// 		port: 465,
+// 		secure: true, // Use SSL
+// 		auth: {
+// 			user: email,
+// 			pass: pass,
+// 		},
+// 		tls: {
+// 			// do not fail on invalid certs
+// 			rejectUnauthorized: false,
+// 		},
+// 	})
+// ); 
+
+// export const mailOptions = {
+// 	from: "contact@mitochonpixel.co.uk",
+// 	to: "contact@mitochonpixel.co.uk",
+// };
+
+
 import nodemailer from "nodemailer";
-import smtpTransport from "nodemailer-smtp-transport";
 
-const email = process.env.EMAIL;
-const pass = process.env.EMAIL_PASS;
-export const transporter = nodemailer.createTransport(
-	smtpTransport({
-		host: "smtpout.secureserver.net",
-		port: 465,
-		secure: true, // Use SSL
-		auth: {
-			user: email,
-			pass: pass,
-		},
-		tls: {
-			// do not fail on invalid certs
-			rejectUnauthorized: false,
-		},
-	})
-); 
-
-export const mailOptions = {
-	from: "contact@mitochonpixel.co.uk",
-	to: "contact@mitochonpixel.co.uk",
-};
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL, // This will reference contact@mitochonpixel.co.uk
+    pass: process.env.EMAIL_PASS, // This will reference your App Password
+  },
+});
